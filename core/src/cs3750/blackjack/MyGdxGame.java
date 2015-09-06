074e8eb3086365bf8f4d46e57a167a2cb7bb37c8
Levi Leavitt <Levi777L@yahoo.com>
2015-09-06 12:03:51 -0600

    Added an array of card images and a couple cards to the board to test.

--- core/src/cs3750/blackjack/MyGdxGame.java
+++ core/src/cs3750/blackjack/MyGdxGame.java
@@ -4,8 +4,9 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
@@ -18,10 +19,21 @@ public class MyGdxGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	OrthographicCamera camera;
 	Texture background;
+	Texture cardSheet;
+	Sprite[][] cards = new Sprite[4][13];
+	
 	
 	@Override
 	public void create () {
 		background = new Texture(Gdx.files.internal("Table.png"));
+		cardSheet = new Texture(Gdx.files.internal("classic-playing-cards.png"));
+		TextureRegion[][] tmp = TextureRegion.split(cardSheet, cardSheet.getWidth()/13, cardSheet.getHeight()/4);
+		for (int i = 0; i < 4; i++){
+			for (int j = 0; j < 13; j++){
+				cards[i][j] = new Sprite(tmp[i][j]);
+			}
+		}
+		
 		System.out.println(background.getWidth() + " " + background.getHeight());
 		camera = new OrthographicCamera();
 		camera.setToOrtho(false, background.getWidth(), background.getHeight());
@@ -40,6 +52,12 @@ public class MyGdxGame extends ApplicationAdapter {
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
 		batch.draw(background, 0, 0);
+		//Temp
+		cards[1][12].setPosition(593, 140);
+		cards[1][11].setPosition(693, 140);
+		cards[1][12].draw(batch);
+		cards[1][11].draw(batch);
+		//end Temp
 		batch.end();
 	}
 	
